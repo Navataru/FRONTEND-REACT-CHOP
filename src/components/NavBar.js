3f2ddc967e5769578410edb859eb70e914e7b895
navatar <umdomby@yandex.ru>
2021-04-27 18:50:37 +0300

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,80 +21,128 @@ const NavBar = observer(() => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Championship Heroes</NavLink>
+                <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Championship Heroes </NavLink>
 
-                <div>
-                    {(() => {
-                        if (user.user.role === "ADMIN") {
-                            return (
-                                <div>
-                                    <Nav className="ml-auto" style={{color: 'white'}}>
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => history.push(ADMIN_SETTINGS)}
-                                        >
-                                            Championship
-                                        </Button>
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => history.push(ADMIN_ROUTE)}
-                                            className="ml-2"
-                                        >
-                                            Registration championship
-                                        </Button>
+                {user.user.role === "ADMIN" ?
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_SETTINGS)}
+                        >
+                            Championship
+                        </Button>
+                    </Nav>
+                    :
+                    <Nav></Nav>
+                }
 
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => logOut()}
-                                            className="ml-2"
-                                        >
-                                            Выйти
-                                        </Button>
-                                    </Nav>
-                                </div>
-                            )
-                        }
-                        else if(user.user.role === "USER"){
-                            return (
-                                <div>
-                                    <Nav className="ml-auto" style={{color: 'white'}}>
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => history.push(ADMIN_ROUTE)}
-                                        >
-                                            Registration championship
-                                        </Button>
+                {user.isAuth ?
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Registration championship
+                        </Button>
 
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => logOut()}
-                                            className="ml-2"
-                                        >
-                                            Выйти
-                                        </Button>
-                                    </Nav>
-                                </div>
-                            )
-
-                        } else {
-                            return (
-                                <div>
-                                    <Nav className="ml-auto" style={{color: 'white'}}>
-                                        <Button
-                                            variant={"outline-light"}
-                                            onClick={() => history.push(LOGIN_ROUTE)}
-                                        >Авторизация
-                                        </Button>
-                                    </Nav>
-                                </div>
-                            )
-                        }
-                    })()}
-                </div>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => logOut()}
+                            className="ml-2"
+                        >
+                            Выйти
+                        </Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
 
     );
+
+    // return (
+    //     <Navbar bg="dark" variant="dark">
+    //         <Container>
+    //             <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Championship Heroes</NavLink>
+    //
+    //             <div>
+    //                 {(() => {
+    //                     if (user.user.role === "ADMIN") {
+    //                         return (
+    //                             <div>
+    //                                 <Nav className="ml-auto" style={{color: 'white'}}>
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => history.push(ADMIN_SETTINGS)}
+    //                                     >
+    //                                         Championship
+    //                                     </Button>
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => history.push(ADMIN_ROUTE)}
+    //                                         className="ml-2"
+    //                                     >
+    //                                         Registration championship
+    //                                     </Button>
+    //
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => logOut()}
+    //                                         className="ml-2"
+    //                                     >
+    //                                         Выйти
+    //                                     </Button>
+    //                                 </Nav>
+    //                             </div>
+    //                         )
+    //                     }
+    //                     else if(user.user.role === "USER"){
+    //                         return (
+    //                             <div>
+    //                                 <Nav className="ml-auto" style={{color: 'white'}}>
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => history.push(ADMIN_ROUTE)}
+    //                                     >
+    //                                         Registration championship
+    //                                     </Button>
+    //
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => logOut()}
+    //                                         className="ml-2"
+    //                                     >
+    //                                         Выйти
+    //                                     </Button>
+    //                                 </Nav>
+    //                             </div>
+    //                         )
+    //
+    //                     } else {
+    //                         return (
+    //                             <div>
+    //                                 <Nav className="ml-auto" style={{color: 'white'}}>
+    //                                     <Button
+    //                                         variant={"outline-light"}
+    //                                         onClick={() => history.push(LOGIN_ROUTE)}
+    //                                     >Авторизация
+    //                                     </Button>
+    //                                 </Nav>
+    //                             </div>
+    //                         )
+    //                     }
+    //                 })()}
+    //             </div>
+    //         </Container>
+    //     </Navbar>
+    //
+    // );
+
+
+
 });
 
 export default NavBar;
